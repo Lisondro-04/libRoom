c545686f4542a973aa2f00a40cee08df93839a74
Lisondro <josuelisondro2304@hotmail.com>
2025-07-07 13:47:20 -0500

    Update settings.py
    added the constants for some variables

--- libRoom_backend/libRoom_backend/settings.py
+++ libRoom_backend/libRoom_backend/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECTS_BASE_DIR = BASE_DIR / "user" / "projects"
+CURRENT_PROJECT_PATH = PROJECTS_BASE_DIR / "current"
+PROJECT_JSON_PATH = CURRENT_PROJECT_PATH / "project.json"
+# para importar from libRoom_backend.settings import PROJECT_JSON_PATH
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
