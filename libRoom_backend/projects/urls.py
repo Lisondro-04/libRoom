9a935e98f35d5f6ee99d03dbb95100246830d2b5
Lisondro <josuelisondro2304@hotmail.com>
2025-07-07 13:46:05 -0500

    Create urls.py
    It contains the url for project view and create project view

---
+++ libRoom_backend/projects/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CreateProjectView, ProjectView
+
+urlpatters = [
+    path('api/project/', ProjectView.as_view(), name='get_project'),
+    path('api/project/create', CreateProjectView.as_view(), name='create_project'),
+]
