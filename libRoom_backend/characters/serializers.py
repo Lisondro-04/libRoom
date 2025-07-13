83d0973f99359573ab623e5510f2017144a823fe
BastienA2005 <basapa13@gmail.com>
2025-07-12 20:22:34 -0500

    Characters

---
+++ libRoom_backend/characters/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+
+class CharacterMetaSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    name = serializers.CharField()
+    type = serializers.ChoiceField(choices=["main", "secondary"])
+    path = serializers.CharField(read_only=True)
+
+
+class CharacterCreateSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    type = serializers.ChoiceField(choices=["main", "secondary"])
+
+
+class SectionPatchSerializer(serializers.Serializer):
+    section = serializers.CharField(help_text="Markdown section title to overwrite")
+    content = serializers.CharField(help_text="New markdown body for that section")
