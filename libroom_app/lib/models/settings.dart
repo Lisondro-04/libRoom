5178340314132337f23bb66429b1e108b34a06e3
Mressence <cordobaguerra19@gmail.com>
2025-07-16 00:07:36 -0500

    screen settings

---
+++ libroom_app/lib/models/settings.dart
@@ -0,0 +1,39 @@
+class Setting {
+  String interfaceLanguage;
+  String interfaceFont; // quitar 'final'
+  int interfaceFontSize;
+  String defaultProjectSaveLocation;
+  String autoSaveFrequency;
+  String defaultExportLocation;
+
+  Setting({
+    required this.interfaceLanguage,
+    required this.interfaceFont,
+    required this.interfaceFontSize,
+    required this.defaultProjectSaveLocation,
+    required this.autoSaveFrequency,
+    required this.defaultExportLocation,
+  });
+
+  factory Setting.fromJson(Map<String, dynamic> json) {
+    return Setting(
+      interfaceLanguage: json['interface_language'],
+      interfaceFont: json['interface_font'],
+      interfaceFontSize: json['interface_font_size'],
+      defaultProjectSaveLocation: json['default_project_save_location'],
+      autoSaveFrequency: json['auto_save_frequency'],
+      defaultExportLocation: json['default_export_location'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'interface_language': interfaceLanguage,
+      'interface_font': interfaceFont,
+      'interface_font_size': interfaceFontSize,
+      'default_project_save_location': defaultProjectSaveLocation,
+      'auto_save_frequency': autoSaveFrequency,
+      'default_export_location': defaultExportLocation,
+    };
+  }
+}
---
+++ libroom_app/lib/services/settings_services.dart
@@ -0,0 +1,31 @@
+// services/settings_service.dart
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/settings.dart';
+
+class SettingsService {
+  final String baseUrl = 'http://TU_IP:8000/api/settings/';
+
+  Future<Setting> fetchSettings() async {
+    final response = await http.get(Uri.parse(baseUrl));
+
+    if (response.statusCode == 200) {
+      return Setting.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load settings');
+    }
+  }
+
+  Future<void> updateSettings(Setting setting) async {
+    final response = await http.put(
+      Uri.parse(baseUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(setting.toJson()),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update settings');
+    }
+  }
+}
